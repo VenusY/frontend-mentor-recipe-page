df6fe8717dd1a995b0f4a2f9654b03d58883dda0
Venus <venusyip@outlook.com>
2025-04-08 23:59:22 +0100

    Style introduction section

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,8 +1,10 @@
+import '../styles/scss/Introduction.scss';
+
 export default function Introduction() {
   return (
     <section className='introduction'>
       <h1 className='introduction__heading'>Simple Omelette Recipe</h1>
-      <p className='introduction__description'>
+      <p className='introduction__description paragraph'>
         An easy and quick dish, perfect for any meal. This classic omelette
         combines beaten eggs cooked to perfection, optionally filled with your
         choice of cheese, vegetables, or meats.
@@ -14,24 +16,24 @@ export default function Introduction() {
         <ul className='time__list'>
           <li className='time__list-item'>
             <div className='time__list-marker'></div>
-            <div className='time__item-container'>
-              <span className='time__category'>Total: </span>
+            <div className='time__item-container paragraph'>
+              <span className='time__category'>Total</span>:{' '}
               <span className='time__minutes'>Approximately 10 minutes</span>
             </div>
           </li>
 
           <li className='time__list-item'>
             <div className='time__list-marker'></div>
-            <div className='time__item-container'>
-              <span className='time__category'>Preparation: </span>
+            <div className='time__item-container paragraph'>
+              <span className='time__category'>Preparation</span>:{' '}
               <span className='time__minutes'>5 minutes</span>
             </div>
           </li>
 
           <li className='time__list-item'>
             <div className='time__list-marker'></div>
-            <div className='time__item-container'>
-              <span className='time__category'>Cooking: </span>
+            <div className='time__item-container paragraph'>
+              <span className='time__category'>Cooking</span>:{' '}
               <span className='time__minutes'>5 minutes</span>
             </div>
           </li>
