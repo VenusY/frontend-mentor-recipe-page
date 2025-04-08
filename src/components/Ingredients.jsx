ba94e54d8f36c55e3b32bc76216b39209319a084
Venus <venusyip@outlook.com>
2025-04-08 21:24:12 +0100

    Refactor ingredients list markup

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -4,14 +4,29 @@ export default function Ingredients() {
       <h2 className='card__heading'>Ingredients</h2>
 
       <ul className='ingredients__list'>
-        <li className='ingredients__ingredient'>2-3 large eggs</li>
-        <li className='ingredients__ingredient'>Salt, to taste</li>
-        <li className='ingredients__ingredient'>Pepper, to taste</li>
-        <li className='ingredients__ingredient'>
-          1 tablespoon of butter or oil
+        <li className='ingredients__list-item'>
+          <div className='ingredients__list-marker'></div>
+          <span className='ingredients__ingredient'>2-3 large eggs</span>
         </li>
-        <li className='ingredients__ingredient'>
-          Optional fillings: cheese, diced vegetables, cooked meats, herbs
+        <li className='ingredients__list-item'>
+          <div className='ingredients__list-marker'></div>
+          <span className='ingredients__ingredient'>Salt, to taste</span>
+        </li>
+        <li className='ingredients__list-item'>
+          <div className='ingredients__list-marker'></div>
+          <span className='ingredients__ingredient'>Pepper, to taste</span>
+        </li>
+        <li className='ingredients__list-item'>
+          <div className='ingredients__list-marker'></div>
+          <span className='ingredients__ingredient'>
+            1 tablespoon of butter or oil
+          </span>
+        </li>
+        <li className='ingredients__list-item'>
+          <div className='ingredients__list-marker'></div>
+          <span className='ingredients__ingredient'>
+            Optional fillings: cheese, diced vegetables, cooked meats, herbs
+          </span>
         </li>
       </ul>
     </section>
