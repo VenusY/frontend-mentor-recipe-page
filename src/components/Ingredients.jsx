1f024cc99650bb7f559b1146c840622e45626289
Venus <venusyip@outlook.com>
2025-04-09 00:20:38 +0100

    Style ingredients section

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -1,30 +1,38 @@
+import '../styles/scss/Ingredients.scss';
+
 export default function Ingredients() {
   return (
-    <section className='ingredients'>
-      <h2 className='card__heading'>Ingredients</h2>
+    <section className='ingredients card__section'>
+      <h2 className='card__heading heading'>Ingredients</h2>
 
       <ul className='ingredients__list'>
         <li className='ingredients__list-item'>
           <div className='ingredients__list-marker'></div>
-          <span className='ingredients__ingredient'>2-3 large eggs</span>
+          <span className='ingredients__ingredient paragraph'>
+            2-3 large eggs
+          </span>
         </li>
         <li className='ingredients__list-item'>
           <div className='ingredients__list-marker'></div>
-          <span className='ingredients__ingredient'>Salt, to taste</span>
+          <span className='ingredients__ingredient paragraph'>
+            Salt, to taste
+          </span>
         </li>
         <li className='ingredients__list-item'>
           <div className='ingredients__list-marker'></div>
-          <span className='ingredients__ingredient'>Pepper, to taste</span>
+          <span className='ingredients__ingredient paragraph'>
+            Pepper, to taste
+          </span>
         </li>
         <li className='ingredients__list-item'>
           <div className='ingredients__list-marker'></div>
-          <span className='ingredients__ingredient'>
+          <span className='ingredients__ingredient paragraph'>
             1 tablespoon of butter or oil
           </span>
         </li>
         <li className='ingredients__list-item'>
           <div className='ingredients__list-marker'></div>
-          <span className='ingredients__ingredient'>
+          <span className='ingredients__ingredient paragraph'>
             Optional fillings: cheese, diced vegetables, cooked meats, herbs
           </span>
         </li>
