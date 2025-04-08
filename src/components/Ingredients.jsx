1c2aead981a1f97bb4578dd1c43c3aae4b34660b
Venus <venusyip@outlook.com>
2025-04-08 09:42:35 +0100

    Add ingredients section markup

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -1,1 +1,19 @@
-export default function Ingredients() {}
+export default function Ingredients() {
+  return (
+    <section className='ingredients'>
+      <h2 className='card__heading'>Ingredients</h2>
+
+      <ul className='ingredients__list'>
+        <li className='ingredients__ingredient'>2-3 large eggs</li>
+        <li className='ingredients__ingredient'>Salt, to taste</li>
+        <li className='ingredients__ingredient'>Pepper, to taste</li>
+        <li className='ingredients__ingredient'>
+          1 tablespoon of butter or oil
+        </li>
+        <li className='ingredients__ingredient'>
+          Optional fillings: cheese, diced vegetables, cooked meats, herbs
+        </li>
+      </ul>
+    </section>
+  );
+}
