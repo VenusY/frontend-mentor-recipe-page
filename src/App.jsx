bd882993dd40e69abbc46fef63ad8b7989d84903
Venus <venusyip@outlook.com>
2025-04-08 09:26:45 +0100

    Add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,15 @@
-export default function App() {}
+import Introduction from './components/Introduction';
+import Ingredients from './components/Ingredients';
+import Instructions from './components/Instructions';
+import Nutrition from './components/Nutrition';
+
+export default function App() {
+  return (
+    <div className='card'>
+      <Introduction />
+      <Ingredients />
+      <Instructions />
+      <Nutrition />
+    </div>
+  );
+}
