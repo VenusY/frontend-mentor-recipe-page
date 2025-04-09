8bdc7f2aa90a5a68af4baf6284500647f164817e
Venus <venusyip@outlook.com>
2025-04-09 20:06:39 +0100

    Style nutrition section

--- src/components/Nutrition.jsx
+++ src/components/Nutrition.jsx
@@ -1,8 +1,10 @@
+import '../styles/scss/Nutrition.scss';
+
 export default function Nutrition() {
   return (
-    <section className='nutrition'>
-      <h2 className='card__heading'>Nutrition</h2>
-      <p className='nutrition__description'>
+    <section className='nutrition card__section'>
+      <h2 className='card__heading heading'>Nutrition</h2>
+      <p className='nutrition__description paragraph'>
         The table below shows nutritional values per serving without the
         additional fillings.
       </p>
@@ -10,19 +12,19 @@ export default function Nutrition() {
       <table className='nutrition-table'>
         <tbody className='nutrition-table__body'>
           <tr className='nutrition-table__row'>
-            <th className='nutrition-table__category'>Calories</th>
+            <th className='nutrition-table__category paragraph'>Calories</th>
             <td className='nutrition-table__value'>277kcal</td>
           </tr>
           <tr className='nutrition-table__row'>
-            <th className='nutrition-table__category'>Carbs</th>
+            <th className='nutrition-table__category paragraph'>Carbs</th>
             <td className='nutrition-table__value'>0g</td>
           </tr>
           <tr className='nutrition-table__row'>
-            <th className='nutrition-table__category'>Protein</th>
+            <th className='nutrition-table__category paragraph'>Protein</th>
             <td className='nutrition-table__value'>20g</td>
           </tr>
           <tr className='nutrition-table__row'>
-            <th className='nutrition-table__category'>Fat</th>
+            <th className='nutrition-table__category paragraph'>Fat</th>
             <td className='nutrition-table__value'>22g</td>
           </tr>
         </tbody>
